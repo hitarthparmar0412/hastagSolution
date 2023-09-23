acf56253cbdbf4dda5e3912bad4ce30222c5d2b3
hitarthparmar0412 <hitarth.parmar0412@gmail.com>
2023-09-23 23:10:37 +0530

    code changes

--- send_email.php
+++ send_email.php
@@ -11,12 +11,12 @@ try {
     //Server settings
     $mail->SMTPDebug = 0;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = '156.unihost.it';  // Specify main and backup SMTP servers
+    $mail->Host = 'smpt.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'hitarth.parmar0412@gmail.com';                 // SMTP username
     $mail->Password = 'hitarth@0412';                           // SMTP password
-    $mail->SMTPSecure = 'STARTTLS';                           
-    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->SMTPSecure = 'ssl';                           
+    $mail->Port = 465;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom('hitarth.parmar0412@gmail.com');
@@ -27,7 +27,7 @@ try {
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = $_POST['subject'];
-    $mail->Body    = $_POST['text'];
+    $mail->Body    = $_POST['message'];
 
     $mail->send();
     header('Location: http://www.example.net/contact.php');
